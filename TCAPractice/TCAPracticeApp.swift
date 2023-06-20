c3724ccc39837d3b79aa0859a3df207279d31687
devanshuchaudhary <52855516+devdchaudhary@users.noreply.github.com>
2023-06-20 09:04:33 +0530

    Initial Commit

---
+++ TCAPractice/TCAPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPracticeApp.swift
+//  TCAPractice
+//
+//  Created by devdchaudhary on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TCAPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
